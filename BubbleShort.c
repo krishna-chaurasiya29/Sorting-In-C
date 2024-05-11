733c5668448461c1a144866c407ade20ec1da172
krishna-chaurasiya29 <chaurasiyakrishna731@gmail.com>
2024-05-11 10:24:42 +0530

    Bubble short in C

---
+++ BubbleShort.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<limits.h>
+#include<conio.h>
+#include<stdbool.h>
+#include<math.h>
+#include<stdlib.h>
+#include<string.h>
+int main()
+{
+    int n;
+    printf("Enter the size of the array\n");
+    scanf("%d",&n);
+    int arr[n];
+    //To determine the size of the arraym if allocating the memory directly
+    // int n=sizeof(arr)/sizeof(arr[0]);
+
+printf("Given Array : \n");
+return 0;
+}
