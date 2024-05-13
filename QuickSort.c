d54eaf102e2f77fdf76897803b56b13610ed3bf0
krishna-chaurasiya29 <chaurasiyakrishna731@gmail.com>
2024-05-13 15:05:11 +0530

    Quick Sort complete efficient code

--- QuickSort.c
+++ QuickSort.c
@@ -15,9 +15,44 @@ for(int i=0;i<n;i++)
 printf("\n");
 }
 
-void QuickSort(int *A, int low, int high)
+int partition(int A[],int low , int high)
 {
-printf(" ");
+    int pivot=A[low];
+    int i=low+1;
+    int j=high;
+    int temp;
+    do
+    {
+        while(A[i]<=pivot)
+        {
+            i++;
+        }
+        while(A[j]>pivot)
+        {
+            j--;
+        }
+        if(i<j)
+        {
+            temp=A[i];
+            A[i]=A[j];
+            A[j]=temp;
+        }
+    }
+        while(i<j);
+        temp=A[low];
+        A[low]=A[j];
+        A[j]=temp;
+        return j;
+}
+void QuickSort(int A[], int low, int high)
+{
+int partitionIndex;
+if(low<high)
+{
+    partitionIndex=partition(A,low,high);
+    QuickSort(A,low,partitionIndex-1);// sort left subarray
+    QuickSort(A,partitionIndex+1,high);//sort right subarray
+}
 }
 int main()
 {
