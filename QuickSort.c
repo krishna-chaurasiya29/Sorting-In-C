963f0d6e7de168b96c3a4270e71f240df7d3f1d6
krishna-chaurasiya29 <chaurasiyakrishna731@gmail.com>
2024-05-13 15:09:43 +0530

    quick sort fully explained code

--- QuickSort.c
+++ QuickSort.c
@@ -70,3 +70,24 @@ QuickSort(A,0,n-1);
 PrintArr(A,n);
 return 0;
 }
+/*
+In the quicksort algorithm, every time you get a fresh unsorted subarray, you do a partition on it. 
+ Partition asks you to first choose an element as a pivot. And as already decided, we would choose the first element of the unsorted subarray as the pivot.
+  We would need two more index variables, i and j. 
+  Below enlisted is the flow of our partition algorithm we must adhere to.
+   We always start from step 1 with each fresh partition call
+*/
+/*
+1 Define i as the low index, which is the index of the first element of the subarray, and j as the high index, which is the index of the last element of the subarray.
+
+2 Set the pivot as the element at the low index i since that is the first index of the unsorted subarray.
+Increase i by 1 until you reach an element greater than the pivot element.
+
+3 Decrease j by 1 until you reach an element smaller than or equal to the pivot element.
+
+4 Having fixed the values of i and j, interchange the elements at indices i and j.
+
+5 Repeat steps 3, 4, and 5 until j becomes less than or equal to i.
+
+6 Finally, swap the pivot element and the element at the index.
+*/
